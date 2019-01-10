7554b31b5a90a246655649c19eff2b2d645c9971
“Jeongwonseok” <jws4281@nate.com>
2019-01-10 14:10:39 +0900

    맵 완성

--- app/src/main/java/com/example/jws/bzapp/MapActivity.java
+++ app/src/main/java/com/example/jws/bzapp/MapActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -48,6 +49,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -67,12 +69,12 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private static final int PERMISSIONS_REQUEST_CODE = 100;
     boolean needRequest = false;
     // 앱을 실행하기 위해 필요한 퍼미션을 정의합니다.
-    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};  // 외부 저장소
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};  // 외부 저장소
     Location mCurrentLocatiion;
     LatLng currentPosition;
     Geocoder geocoder;
-    double mLat=37.56;
-    double mLong=126.97;
+    double mLat = 37.56;
+    double mLong = 126.97;
     private FusedLocationProviderClient mFusedLocationClient;
     private LocationRequest locationRequest;
     private Location location;
@@ -86,7 +88,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     Marker mCenterMarker;
     private View mLayout;  // Snackbar 사용하기 위해서는 View가 필요합니다.
     // (참고로 Toast에서는 Context가 필요했습니다.)
-
+    int a = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -114,7 +116,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         btnResult = (Button) findViewById(R.id.btnResult);
         btnBack = (ImageButton) findViewById(R.id.btnBack);
         editSearch = (EditText) findViewById(R.id.locationSearch);
-        check=(Button)findViewById(R.id.checkbtn);
+        check = (Button) findViewById(R.id.checkbtn);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -126,8 +128,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         navigationView.setNavigationItemSelectedListener(this);
 
 
-
-
         //GSP환경을 체크해서 자기위치를 뛰운다.
         check.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -137,7 +137,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
             }
         });
-
 
 
         btnResult.setOnClickListener(new View.OnClickListener() {
@@ -187,10 +186,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(search, 16));
 
 
-
             }
         });
-
 
 
         ///////////////////////////////////스피너//////////////////////////////////
@@ -211,9 +208,29 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String text = spinner.getSelectedItem().toString();
                 if (text.equals("100m")) {
-//                    onAddCircle(100);
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.zoomTo(18);
+                    mGoogleMap.moveCamera(cameraUpdate);
+                    mGoogleMap.clear();
+                    a = 100;
+                    onAddCircle100(a);
                 } else if (text.equals("200m")) {
-//                    onAddCircle(200);
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.zoomTo(17);
+                    mGoogleMap.moveCamera(cameraUpdate);
+                    mGoogleMap.clear();
+                    a = 200;
+                    onAddCircle200(a);
+                }  else if (text.equals("500m")) {
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.zoomTo(16);
+                    mGoogleMap.moveCamera(cameraUpdate);
+                    mGoogleMap.clear();
+                    a = 500;
+                    onAddCircle500(a);
+                } else if (text.equals("1km")) {
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.zoomTo(15);
+                    mGoogleMap.moveCamera(cameraUpdate);
+                    mGoogleMap.clear();
+                    a = 1000;
+                    onAddCircle1000(a);
                 }
 
             }
@@ -280,10 +297,10 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             if (locationList.size() > 0) {
                 location = locationList.get(locationList.size() - 1);
                 //location = locationList.get(0);
-                mLat=location.getLatitude();
-                mLong=location.getLongitude();
+                mLat = location.getLatitude();
+                mLong = location.getLongitude();
                 currentPosition
-                        = new LatLng(mLat,mLong);
+                        = new LatLng(mLat, mLong);
                 String markerTitle = getCurrentAddress(currentPosition);
                 //현재 위치에 마커 생성하고 이동
                 setCurrentLocation(location);
@@ -296,14 +313,13 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     };
 
 
-
     private void startLocationUpdates() {
 
         if (!checkLocationServicesStatus()) { //GPS 설정 상태가 x면
 
             Log.d(TAG, "startLocationUpdates : call showDialogForLocationServiceSetting");
             showDialogForLocationServiceSetting();
-        }else {
+        } else {
 
             int hasFineLocationPermission = ContextCompat.checkSelfPermission(this,
                     Manifest.permission.ACCESS_FINE_LOCATION);
@@ -311,17 +327,16 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     Manifest.permission.ACCESS_COARSE_LOCATION);
 
             if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED ||
-                    hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED   ) {
+                    hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED) {
                 return;
             }
 
             if (checkPermission())
                 mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null); //자기 우치 받아오는 코드
-            if (mGoogleMap!=null)
+            if (mGoogleMap != null)
                 mGoogleMap.setMyLocationEnabled(true);
         }
     }
-
 
 
     @Override
@@ -331,6 +346,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         //런타임 퍼미션 요청 대화상자나 GPS 활성 요청 대화상자 보이기전에
         //지도의 초기위치를 서울로 이동
         setDefaultLocation();
+        onAddCircle100(a);
 
         //런타임 퍼미션 처리
         // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
@@ -340,13 +356,12 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 Manifest.permission.ACCESS_COARSE_LOCATION);
 
 
-
         if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
-                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED   ) {
+                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
             // 2. 이미 퍼미션을 가지고 있다면
             // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
             startLocationUpdates(); // 3. 위치 업데이트 시작
-        }else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
 
             // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
@@ -358,14 +373,14 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     public void onClick(View view) {
 
                         // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
-                        ActivityCompat.requestPermissions( MapActivity.this, REQUIRED_PERMISSIONS,
+                        ActivityCompat.requestPermissions(MapActivity.this, REQUIRED_PERMISSIONS,
                                 PERMISSIONS_REQUEST_CODE);
                     }
                 }).show();
             } else {
                 // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
                 // 요청 결과는 onRequestPermissionResult에서 수신됩니다.A
-                ActivityCompat.requestPermissions( this, REQUIRED_PERMISSIONS,
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
                         PERMISSIONS_REQUEST_CODE);
             }
         }
@@ -377,7 +392,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             @Override
             public void onMapClick(LatLng latLng) {
 
-                Log.d( TAG, "onMapClick :");
+                Log.d(TAG, "onMapClick :");
             }
         });
         mGoogleMap.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {   //카메라 움직일때마다 실행되는 메소드
@@ -390,17 +405,26 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 }
 
                 CameraPosition test = mGoogleMap.getCameraPosition();
-                mLat=mGoogleMap.getCameraPosition().target.latitude;
-                mLong=mGoogleMap.getCameraPosition().target.longitude;
+                mLat = mGoogleMap.getCameraPosition().target.latitude;
+                mLong = mGoogleMap.getCameraPosition().target.longitude;
                 //Assign mCenterMarker reference:
+
+                if (a==100) {
+                    mGoogleMap.clear();
+                    onAddCircle100(a);
+                } else if (a==200) {
+                    mGoogleMap.clear();
+                    onAddCircle200(a);
+                } else if (a==500) {
+                    mGoogleMap.clear();
+                    onAddCircle500(a);
+                } else if (a==1000) {
+                    mGoogleMap.clear();
+                    onAddCircle1000(a);
+                }
             }
         });
     }
-
-
-
-
-
 
 
     public String getCurrentAddress(LatLng latlng) {
@@ -450,9 +474,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     public void setCurrentLocation(Location location) {
 
 
-
-
-
         LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
 
         MarkerOptions markerOptions = new MarkerOptions();
@@ -460,8 +481,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 //        markerOptions.title(markerTitle);
 //        markerOptions.snippet(markerSnippet);
         markerOptions.draggable(true);
-
-
 
 
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(currentLatLng);
@@ -474,7 +493,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
 
         //디폴트 위치, Seoul
-        LatLng DEFAULT_LOCATION = new LatLng(mLat,mLong);
+        LatLng DEFAULT_LOCATION = new LatLng(mLat, mLong);
 
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, 16);
         mGoogleMap.moveCamera(cameraUpdate);
@@ -491,16 +510,14 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 Manifest.permission.ACCESS_COARSE_LOCATION);
 
 
-
         if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
-                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED   ) {
+                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
             return true;
         }
 
         return false;
 
     }
-
 
 
     /*
@@ -511,7 +528,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                                            @NonNull String[] permissions,
                                            @NonNull int[] grandResults) {
 
-        if ( permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
 
             // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
 
@@ -528,12 +545,11 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             }
 
 
-            if ( check_result ) {
+            if (check_result) {
 
                 // 퍼미션을 허용했다면 위치 업데이트를 시작합니다.
                 startLocationUpdates();
-            }
-            else {
+            } else {
                 // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
 
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
@@ -551,7 +567,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                         }
                     }).show();
 
-                }else {
+                } else {
 
 
                     // "다시 묻지 않음"을 사용자가 체크하고 거부를 선택한 경우에는 설정(앱 정보)에서 퍼미션을 허용해야 앱을 사용할 수 있습니다.
@@ -603,7 +619,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -626,18 +641,64 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
 
-//
-//    public void onAddCircle(int a){
-//        LatLng position = new LatLng(mLat,mLong);
-//
-//        //나의 위치 마커
-////        MarkerOptions mymarker = new MarkerOptions().position(position);
-//
-//        //사이클 반경 설정
-//        CircleOptions circle = new CircleOptions().center(position).radius(a)//반경
-//                .strokeColor(0)//선 넓이
-//                .fillColor(Color.parseColor("#880000ff"));
-////        this.mMap.addMarker(mymarker);
-//        this.mGoogleMap.addCircle(circle);
-//    }
+    //
+    public void onAddCircle100(int a) {
+        LatLng position = new LatLng(mLat, mLong);
+
+        //나의 위치 마커
+        //MarkerOptions mymarker = new MarkerOptions().position(position);
+
+        //사이클 반경 설정
+        CircleOptions circle = new CircleOptions().center(position).radius(a)//반경
+                .strokeWidth(3)//선 넓이
+                .fillColor(0x44808080)
+                .strokeColor(Color.GRAY);
+        //this.mGoogleMap.addMarker(mymarker);
+        this.mGoogleMap.addCircle(circle);
+    }
+
+    public void onAddCircle200(int a) {
+        LatLng position = new LatLng(mLat, mLong);
+
+        //나의 위치 마커
+        //MarkerOptions mymarker = new MarkerOptions().position(position);
+
+        //사이클 반경 설정
+        CircleOptions circle = new CircleOptions().center(position).radius(a)//반경
+                .strokeWidth(3)//선 넓이
+                .fillColor(0x44808080)
+                .strokeColor(Color.GRAY);
+        //this.mGoogleMap.addMarker(mymarker);
+        this.mGoogleMap.addCircle(circle);
+    }
+
+    public void onAddCircle500(int a) {
+        LatLng position = new LatLng(mLat, mLong);
+
+        //나의 위치 마커
+        //MarkerOptions mymarker = new MarkerOptions().position(position);
+
+        //사이클 반경 설정
+        CircleOptions circle = new CircleOptions().center(position).radius(a)//반경
+                .strokeWidth(3)//선 넓이
+                .fillColor(0x44808080)
+                .strokeColor(Color.GRAY);
+        //this.mGoogleMap.addMarker(mymarker);
+        this.mGoogleMap.addCircle(circle);
+    }
+
+    public void onAddCircle1000(int a) {
+        LatLng position = new LatLng(mLat, mLong);
+
+        //나의 위치 마커
+        //MarkerOptions mymarker = new MarkerOptions().position(position);
+
+        //사이클 반경 설정
+        CircleOptions circle = new CircleOptions().center(position).radius(a)//반경
+                .strokeWidth(3)//선 넓이
+                .fillColor(0x44808080)
+                .strokeColor(Color.GRAY);
+        //this.mGoogleMap.addMarker(mymarker);
+        this.mGoogleMap.addCircle(circle);
+    }
 }
