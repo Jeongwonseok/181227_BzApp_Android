6c302a66a5cfc57c014d7c139fbb680ca4bfed3b
yeol <nwiz9428@naver.com>
2019-01-17 19:07:26 +0900

    주거인구
    주거인구 테스트

---
+++ app/src/main/java/com/example/jws/bzapp/FindApopulation.java
@@ -0,0 +1,22 @@
+package com.example.jws.bzapp;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindApopulation extends StringRequest {
+    final static private String URL = "http://qwerr784.cafe24.com/FindApopulation.php";
+    private Map<String,String> parameters;
+
+    public FindApopulation(String location, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("location",location);
+    }
+
+    public Map<String, String> getParams(){
+        return parameters;
+    }
+}
