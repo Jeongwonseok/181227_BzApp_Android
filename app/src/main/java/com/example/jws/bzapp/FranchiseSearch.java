de3e59d2a6698b3c7158ed4def311ce02cfe45c4
dkwlfowh <37256341+dkwlfowh@users.noreply.github.com>
2019-03-13 13:50:08 +0900

    프랜차니즈

---
+++ app/src/main/java/com/example/jws/bzapp/FranchiseSearch.java
@@ -0,0 +1,16 @@
+package com.example.jws.bzapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class FranchiseSearch  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_franchsearch);
+    }
+
+}
