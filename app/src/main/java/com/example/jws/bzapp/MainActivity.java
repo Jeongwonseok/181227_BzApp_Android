50e21765d1f74708d450e329d48955fbf826218c
“Jeongwonseok” <jws4281@nate.com>
2019-03-19 03:02:02 +0900

    일단 완성... - 광고

--- app/src/main/java/com/example/jws/bzapp/MainActivity.java
+++ app/src/main/java/com/example/jws/bzapp/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.text.Layout;
 import android.util.Base64;
 import android.util.Log;
@@ -24,6 +25,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Adapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -52,6 +54,9 @@ public class MainActivity extends AppCompatActivity
 
     Boolean logincheck, surveycheck;
     String loginID;
+    FlipAdapter flipadapter;
+    ViewPager viewPager;
+
 
 
     private static final String TAG = "Login";
@@ -63,6 +68,10 @@ public class MainActivity extends AppCompatActivity
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.view);
+        flipadapter = new FlipAdapter(this);
+        viewPager.setAdapter(flipadapter);
 
         //로그인 체크와 아이디 값 가져옥;
         SharedPreferences test = getSharedPreferences("check", Activity.MODE_PRIVATE);
