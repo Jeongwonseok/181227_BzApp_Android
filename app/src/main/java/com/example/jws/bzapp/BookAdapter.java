10ae9299ed206ba6a7176354096a6cd4f4fd995e
yeol <nwiz9428@naver.com>
2019-04-07 11:51:27 +0900

    인터페이스 분리
    인터페이스분리

--- app/src/main/java/com/example/jws/bzapp/BookAdapter.java
+++ app/src/main/java/com/example/jws/bzapp/BookAdapter.java
@@ -2,16 +2,11 @@ package com.example.jws.bzapp;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -82,8 +77,4 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> im
             mListener = onListItemClickListener;
         }
     }
-}
-
-interface OnListItemClickListener {
-    public void onListItemClick(int position);
 }
---
+++ app/src/main/java/com/example/jws/bzapp/OnListItemClickListener.java
@@ -0,0 +1,6 @@
+package com.example.jws.bzapp;
+
+
+interface OnListItemClickListener {
+    public void onListItemClick(int position);
+}
