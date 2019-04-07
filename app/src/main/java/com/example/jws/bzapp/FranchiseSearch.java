939f414efb7a9ef22ca4da1e37f4b042f3185bcf
dkwlfowh <37256341+dkwlfowh@users.noreply.github.com>
2019-04-07 12:56:58 +0900

    자동완ㄴ성끝냄

--- app/src/main/java/com/example/jws/bzapp/FranchiseSearch.java
+++ app/src/main/java/com/example/jws/bzapp/FranchiseSearch.java
@@ -24,13 +24,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FranchiseSearch  extends AppCompatActivity {
-           // 데이터를 넣은 리스트변수
+    // 데이터를 넣은 리스트변수
     private ListView listView;          // 검색을 보여줄 리스트변수
     private EditText Search;        // 검색어를 입력할 Input 창
     private SearchAdapter adapter;      // 리스트뷰에 연결할 아답터
     private List<ListVO> list;
     private ArrayList<ListVO> arraylist;
     ImageButton btnBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +40,7 @@ public class FranchiseSearch  extends AppCompatActivity {
         Search = (EditText) findViewById(R.id.fSearch);
         listView = (ListView) findViewById(R.id.listView);
         list = new ArrayList<ListVO>();
-        btnBack = (ImageButton)findViewById(R.id.btnBack);
+        btnBack = (ImageButton) findViewById(R.id.btnBack);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,14 +49,23 @@ public class FranchiseSearch  extends AppCompatActivity {
         });
         settingList();
 
-        arraylist.addAll(list);
-
-        // 리스트에 연동될 아답터를 생성한다.
-        adapter = new SearchAdapter(list,this);
-        // 리스트뷰에 아답터를 연결한다.
+        arraylist = new ArrayList<ListVO>();
+        adapter = new SearchAdapter(list, this);
         listView.setAdapter(adapter);
-        // input창에 검색어를 입력시 "addTextChangedListener" 이벤트 리스너를 정의한다.
-        editSearch.addTextChangedListener(new TextWatcher() {
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String top = "";
+                Intent intent = new Intent(getApplicationContext(), Franchisedetail.class);
+                intent.putExtra("Shopname", list.get(position).getTitle());
+                intent.putExtra("Category", list.get(position).getCategory());
+                startActivity(intent);
+            }
+        });
+
+
+        Search.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
@@ -67,50 +77,41 @@ public class FranchiseSearch  extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                // input창에 문자를 입력할때마다 호출된다.
-                // search 메소드를 호출한다.
-                String text = editSearch.getText().toString();
+
+                String text = Search.getText().toString();
                 search(text);
             }
         });
 
 
-
     }
 
-    // 검색을 수행하는 메소드
+
     public void search(String charText) {
-
-        // 문자 입력시마다 리스트를 지우고 새로 뿌려준다.
         list.clear();
-
-        // 문자 입력이 없을때는 모든 데이터를 보여준다.
         if (charText.length() == 0) {
             list.addAll(arraylist);
-        }
-        // 문자 입력을 할때..
-        else
-        {
-            // 리스트의 모든 데이터를 검색한다.
-            for(int i = 0;i < arraylist.size(); i++)
-            {
-                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
-                if (arraylist.get(i).getTitle().contains(charText))
-                {
-                    // 검색된 데이터를 리스트에 추가한다.
+        } else {
+            for (int i = 0; i < arraylist.size(); i++) {
+                if (arraylist.get(i).getTitle().contains(charText)) {
                     ListVO listVO = new ListVO();
-                    listVO.setImg(arraylist.get(i).getImg());
+                    listVO.setCategory(arraylist.get(i).getCategory());
                     listVO.setTitle(arraylist.get(i).getTitle());
                     listVO.setContext(arraylist.get(i).getContext());
                     list.add(listVO);
                 }
             }
         }
-        // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
         adapter.notifyDataSetChanged();
     }
 
-        class Search extends AsyncTask<Void, Void, String> {
+
+    private void settingList() {
+        Search SearchList = new Search();
+        SearchList.execute();
+    }
+
+    class Search extends AsyncTask<Void, Void, String> {
         @Override
         protected void onPreExecute() {
         }
@@ -143,13 +144,14 @@ public class FranchiseSearch  extends AppCompatActivity {
         protected void onProgressUpdate(Void... values) {
             super.onProgressUpdate(values);
         }
+
         //결과값 출력 메소드
         public void show(String s) {
             try {
                 JSONObject jsonObject = new JSONObject(s);
                 JSONArray jsonArray = jsonObject.getJSONArray("response");
                 int count = 0;
-                String Name,Mutual,Category;
+                String Name, Mutual, Category;
                 while (count < jsonArray.length()) {
                     JSONObject object = jsonArray.getJSONObject(count);
                     Name = object.getString("Name");
@@ -167,10 +169,10 @@ public class FranchiseSearch  extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+
         @Override
         protected void onPostExecute(String s) {
             show(s);
         }
     }
-
 }
