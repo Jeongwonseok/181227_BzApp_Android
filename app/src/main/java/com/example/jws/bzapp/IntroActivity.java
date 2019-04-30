34ded0e4ab3af0ea2c2ba49d56b4e742ced2b8ae
“Jeongwonseok” <jws4281@nate.com>
2019-04-30 18:22:40 +0900

    인트론 초안

---
+++ app/src/main/java/com/example/jws/bzapp/IntroActivity.java
@@ -0,0 +1,29 @@
+package com.example.jws.bzapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class IntroActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_layout); //xml , java 소스 연결
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent (getApplicationContext(), MainActivity.class);
+                startActivity(intent); //다음화면으로 넘어감
+                finish();
+            }
+        },500); //3초 뒤에 Runner객체 실행하도록 함
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
